bf5c2fdf04c1cab2d20114f2efc5b28e0c5f1c1d
lennysgarage <lennysgarage1@gmail.com>
2022-05-23 23:29:19 -0400

    feat: update name of portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Landing from './components/Landing'
 import Resume from './components/Resume';
 
