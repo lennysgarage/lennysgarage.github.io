78b1f68d9748945b8cd8110f3b2f18e3a45c5698
lennysgarage <lennysgarage1@gmail.com>
2022-07-04 22:50:24 -0400

    Update PyJaC Image
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- src/myData.js
+++ src/myData.js
@@ -16,7 +16,6 @@ export default data = {
             id: 0,
             title: "WikiLink",
             short: "Extend any URL using a random wikipedia article's contents.",
-            tech: "NodeJS, ReactJS",
             imageSrc: "assets/wikilink.png",
             git: "https://github.com/lennysgarage/WikiLink",
             url: "https://wikifylink.me/"
@@ -25,7 +24,6 @@ export default data = {
           id: 1,
           title: "Discord Memories",
           short: "A Discord bot for exploring past memories.",
-          tech: "NodeJS, DiscordJS",
           imageSrc: "assets/maroon5.jpg",
           git: "https://github.com/lennysgarage/DiscordMemories",
           url: "https://top.gg/bot/867469054931501078"
@@ -34,7 +32,6 @@ export default data = {
         id: 2,
         title: "Letterboxd Scraper",
         short: "A web scraper tool to download a letterboxd watchlist or other public list.",
-        tech: "Golang, HTML, JS",
         imageSrc: "assets/letterboxdscraper.png",
         git: "https://github.com/lennysgarage/letterboxd-scraper",
         url: "https://taupe-lollipop-099fa5.netlify.app/"
@@ -43,7 +40,6 @@ export default data = {
         id: 3,
         title: "Letterboxd Picker",
         short: "A web tool to pick a movie off your letterboxd watchlist.",
-        tech: "Golang, HTML, JS",
         imageSrc: "assets/letterboxdpicker.png",
         git: "https://github.com/lennysgarage/letterboxd-picker",
         url: "https://willowy-cucurucho-2a592f.netlify.app/"
@@ -52,8 +48,7 @@ export default data = {
         id: 4,
         title: "PyJaC-IEEE",
         short: "A responsive IEEE-754 to Decimal and Decimal to IEEE-754 single precision converter.",
-        tech: "Java, JavaFX",
-        imageSrc: "assets/pyjac.png",
+        imageSrc: "assets/IEEE-754.jpg",
         git: "https://github.com/lennysgarage/PyJaC-IEEE",
         url: "https://github.com/lennysgarage/PyJaC-IEEE"
     },
