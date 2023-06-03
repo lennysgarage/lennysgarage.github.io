ddb8f1de25c135ed5c9c29c2fb4e12e381e9bec4
lennysgarage <lennysgarage1@gmail.com>
2023-06-03 16:23:53 -0400

    fix: remove references to resume.jsx
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Landing from './components/Landing'
-import Resume from './components/Resume';
 
 function App() {
   return (
